c91a8d848338857626af1889ac30eb5347a5dd6b
Hasier Pastor <hasier.pastor@gmail.com>
2019-05-17 15:45:58 -0700

    imported react-window - created virtualized list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { FixedSizeList as List } from 'react-window';
+const axios = require('axios');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URL = 'https://api.spacexdata.com/v3/launches';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      launches: []
+    };
+  }
+
+  componentDidMount = async () => {
+    try {
+      let resp = await axios.get(URL);
+      let launches = resp.data;
+      this.setState({ launches, isLoading: false });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    const Row = ({ index, style }) => (
+      <div className={index % 2 ? 'launch-odd' : 'launch-even'} style={style}>
+        {this.state.launches[index].mission_name}
+      </div>
+    );
+
+    if (this.state.isLoading) {
+      return <div>LOADING...</div>;
+    }
+
+    return (
+      <List
+        className="launches-list"
+        height={150}
+        itemCount={this.state.launches.length}
+        itemSize={35}
+        width={300}
+      >
+        {Row}
+      </List>
+    );
+  }
 }
 
 export default App;
