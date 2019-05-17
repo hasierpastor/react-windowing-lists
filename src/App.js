b8de18baadd392e585f254bf501e0288ee9fb649
Hasier Pastor <hasier.pastor@gmail.com>
2019-05-17 15:55:36 -0700

    refactored and added comments

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
     };
   }
 
+  //make api call to get list launches and add to state
   componentDidMount = async () => {
     try {
       let resp = await axios.get(URL);
@@ -24,20 +25,23 @@ class App extends Component {
   };
 
   render() {
+    //Row will be passed into react-window list component
     const Row = ({ index, style }) => (
       <div className={index % 2 ? 'launch-odd' : 'launch-even'} style={style}>
         {this.state.launches[index].mission_name}
       </div>
     );
 
+    //if launches have not been received displayed loading => once received will return list
     if (this.state.isLoading) {
       return <div>LOADING...</div>;
     }
 
+    //return react-window list with Row passed into it
     return (
       <List
         className="launches-list"
-        height={150}
+        height={400}
         itemCount={this.state.launches.length}
         itemSize={35}
         width={300}
